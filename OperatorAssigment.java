ced66e519fe945a7505b01f797462929f83d469c
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-07-09 10:07:50 +0700

    Operator assigment

---
+++ OperatorAssigment.java
@@ -0,0 +1,29 @@
+package com.tutorial;
+
+import javax.swing.text.Style;
+
+public class OperatorAssigment {
+    public static void main(String[] args){
+        //Operator Assigment
+
+        int a=1;
+        a = a + 3;
+        System.out.println(a);
+
+        int b=3;
+        b+=5;
+        System.out.println(b);
+
+        b-=4;
+        System.out.println(b);
+
+        b*=3;
+        System.out.println(b);
+
+        b/=2;
+        System.out.println(b);
+
+        b%=5;
+        System.out.println(b);
+    }
+}
