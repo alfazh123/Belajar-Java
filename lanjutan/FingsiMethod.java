7271606208e00ad326523955a8e2381d33f58122
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-07-12 14:52:42 +0700

    Fungsi/Method

--- lanjutan/FingsiMethod.java
+++ lanjutan/FingsiMethod.java
@@ -1,7 +1,27 @@
 package com.tutorial.lanjutan;
 
+import java.util.Scanner;
+
 public class FingsiMethod {
     public static void main(String[] args){
+        //Menghitung luas persegi dengan fungsi
+        int panjang,lebar,Luas;
 
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang : ");
+        panjang = inputUser.nextInt();
+        System.out.print("Masukkan Lebar : ");
+        lebar = inputUser.nextInt();
+
+        Luas = hitung(panjang,lebar);
+        System.out.printf("Luas persegi dengan panjang '%d' dan lebar '%d' adalah '%d'",panjang,lebar,Luas);
+
+    }
+
+    public static int hitung(int panjang, int lebar){
+        int luas = panjang * lebar;
+
+        return luas;
     }
 }
